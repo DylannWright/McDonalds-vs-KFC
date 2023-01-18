f8baebd8dff8e80c6aa0dcdc703dc3c9d0c60e00
Dylan Wright <17WrighD@fulford.york.sch.uk>
2023-01-18 11:22:46 +0000

    Made a theme switcher

--- lib.js
+++ lib.js
@@ -1,1 +1,16 @@
-
+function toggleTheme() {
+    
+    var theme = document.getElementsByTagName('link')[0];
+    var logo = document.getElementById('logo');
+    var topHeader = document.getElementById('topHeader');
+    
+    if (theme.getAttribute('href') == 'KFStyles.css') {
+        theme.setAttribute('href', 'McStyles.css');
+        logo.src = "https://1000logos.net/wp-content/uploads/2017/03/McDonalds-logo.png"
+        topHeader.innerText = "Why McDonalds is better than KFC"
+    } else {
+        theme.setAttribute('href', 'KFStyles.css');
+        logo.src = "https://1000logos.net/wp-content/uploads/2017/03/Kfc_logo.png"
+        topHeader.innerText = "Why KFC is better than McDonalds"
+    }
+} 
